56b05e01101c8ce9557e95e7c3f6893c3bc30060
subinio <subinjin22@gmail.com>
2021-02-08 02:23:26 +0900

    python anywhere2

--- project/project/settings.py
+++ project/project/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
 SECRET_KEY = '*7top6ahe#57&s9lkg!6#d8_5^wiktnijzio4^w1=%jn%r*@$-'
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
 
 
 # Application definition
